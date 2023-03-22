9a528955036b3cbee211e35499e05ac27d86fefa
KChino <kevintlapizaga@gmail.com>
2023-03-22 12:04:52 -0300

    hooks and validate
    hooks before Create-Update for hash password.
    Also,beforeBulkCreate.

--- models/User.js
+++ models/User.js
@@ -1,4 +1,5 @@
 const { Model, DataTypes } = require("sequelize");
+const bcrypt = require("bcryptjs");
 
 class User extends Model {
   static initModel(sequelize) {
@@ -8,38 +9,70 @@ class User extends Model {
           type: DataTypes.BIGINT.UNSIGNED,
           primaryKey: true,
           autoIncrement: true,
+          set() {
+            throw new Error("Do not try to set the id value!");
+          },
         },
         firstname: {
           type: DataTypes.STRING,
           allowNull: false,
+          validate: {
+            notNull: { msg: "firstname is required" },
+            notEmpty: true,
+          },
         },
         lastname: {
           type: DataTypes.STRING,
           allowNull: false,
+          validate: {
+            notNull: { msg: "lastname is required" },
+            notEmpty: true,
+          },
         },
         email: {
           type: DataTypes.STRING,
+          unique: true,
           allowNull: false,
+          validate: {
+            notNull: { msg: "email is required" },
+            notEmpty: true,
+            isEmail: true,
+          },
         },
         address: {
           type: DataTypes.STRING,
           allowNull: false,
+          validate: {
+            notNull: { msg: "address is required" },
+            notEmpty: true,
+          },
         },
         password: {
           type: DataTypes.STRING,
           allowNull: false,
+          validate: {
+            notNull: { msg: "password is required" },
+            notEmpty: true,
+          },
         },
         phone: {
           type: DataTypes.STRING,
           allowNull: false,
+          validate: {
+            notNull: { msg: "phone is required" },
+            notEmpty: true,
+          },
         },
         avatar: {
           type: DataTypes.STRING,
-          allowNull: false,
         },
         birthdate: {
           type: DataTypes.DATE,
           allowNull: false,
+          validate: {
+            notNull: { msg: "birthdate is required" },
+            isDate: true,
+          },
         },
       },
       {
@@ -49,6 +82,25 @@ class User extends Model {
     );
     return User;
   }
+  async isValidPassword(password) {
+    return await bcrypt.compare(password, this.password);
+  }
 }
+
+User.beforeCreate(async (user) => {
+  const hashedPassword = await bcrypt.hash(user.password, 12);
+  user.password = hashedPassword;
+});
+User.beforeUpdate(async (user) => {
+  const hashedPassword = await bcrypt.hash(user.password, 12);
+  user.password = hashedPassword;
+});
+
+User.beforeBulkCreate(async (users) => {
+  users.map(async (user) => {
+    const hashedPassword = await bcrypt.hash(user.password, 12);
+    user.password = hashedPassword;
+  });
+});
 
 module.exports = User;
--- server.js
+++ server.js
@@ -2,11 +2,12 @@ require("dotenv").config();
 
 const express = require("express");
 const routes = require("./routes");
-const APP_PORT = process.env.APP_PORT || 3000;
+const APP_PORT = process.env.APP_PORT || 8000;
 const app = express();
 
 /* app.use(methodOverride("_method")); */
 app.use(express.static("public"));
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 
