92e9adb80c98ccd20de694ec691322e6ccd05aeb
KChino <kevintlapizaga@gmail.com>
2023-03-23 11:39:49 -0300

    Update userSeeder.js

--- seeders/userSeeder.js
+++ seeders/userSeeder.js
@@ -44,5 +44,5 @@ module.exports = async () => {
   }
 
   await User.bulkCreate(users);
-  console.log("[Database] Se corrió el seeder de Articles.");
+  console.log("[Database] Se corrió el seeder de Users.");
 };
