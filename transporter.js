00a81f650c466b14d75ae373555e5dda81cdfd26
KChino <kevintlapizaga@gmail.com>
2023-04-06 13:57:55 -0300

    Update transporter.js

--- transporter.js
+++ transporter.js
@@ -1,12 +1,10 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-  service: `gmail`,
-  port: 465,
-  secure: true,
+  service: "gmail",
   auth: {
-    user: process.env.EMAIL_USER, // generated ethereal user
-    pass: process.env.EMAIL_APP_PASS, // generated ethereal password
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_APP_PASS,
   },
 });
 
