2e768b780b58f1e82296fce32ffa106b6b208183
KChino <kevintlapizaga@gmail.com>
2023-04-11 10:50:24 -0300

    Update userSeeder.js

--- seeders/userSeeder.js
+++ seeders/userSeeder.js
@@ -11,6 +11,42 @@ module.exports = async () => {
       phone: "611385728",
       avatar: "Profile_defaultFemale.png",
     },
+    {
+      firstname: "Santiago",
+      lastname: "Fernández",
+      email: "santiago.fernandez@gmail.com",
+      password: "1234",
+      address: "Calle de la Paz 15",
+      phone: "673842675",
+      avatar: "Profile_defaultMale.png",
+    },
+    {
+      firstname: "Andrea",
+      lastname: "Hernández",
+      email: "andrea.hernandez@gmail.com",
+      password: "1234",
+      address: "Calle del Cid 3",
+      phone: "611324568",
+      avatar: "Profile_defaultFemale.png",
+    },
+    {
+      firstname: "David",
+      lastname: "Pérez",
+      email: "david.perez@gmail.com",
+      password: "1234",
+      address: "Calle de la Estrella 12",
+      phone: "689345678",
+      avatar: "Profile_defaultMale.png",
+    },
+    {
+      firstname: "Alicia",
+      lastname: "González",
+      email: "alicia.gonzalez@gmail.com",
+      password: "1234",
+      address: "Avenida de la Constitución 8",
+      phone: "678567894",
+      avatar: "Profile_defaultFemale.png",
+    },
   ];
 
   await User.bulkCreate(users);
