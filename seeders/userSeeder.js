4089e74ae02586fc4cb5caedfe4c6e867c519c89
KChino <kevintlapizaga@gmail.com>
2023-04-11 10:03:03 -0300

    reset

--- seeders/userSeeder.js
+++ seeders/userSeeder.js
@@ -1,25 +1,3 @@
-/**
- * El seeder no es más que un archivo que contiene una función que se encarga
- * de insertar datos (generalmente de prueba) en una base de datos.
- *
- * El nombre "seeder" es una convención y significa "semillero".
- *
- * Además, en este caso, se está usando una librería llamada Faker
- * (https://fakerjs.dev/) para facilitar la creación de datos ficticios como
- * nombres, apellidos, títulos, direcciones y demás textos.
- *
- * Suele ser común que en los seeders exista un `for` donde se define la
- * cantidad de registros de prueba que se insertarán en la base de datos.
- * En este ejemplo se están insertando 3000 artículos con textos ficticios.
- *
- * A priori, esta función debería ser ejecutada una única vez, al correr el
- * proyecto. Esto está hecho el archivo dbInitialSetup.js. Para evitar que se
- * corran los seeders se podría borrar o comentar dicha línea. Como alternativa
- * se podría escribir un código que permita ejectuar los seeders desde la
- * terminal.
- *
- */
-
 const { User } = require("../models");
 
 module.exports = async () => {
