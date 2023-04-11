293eeb2b9d9a4792478b4b9a3a99067d7dfac5d3
KChino <kevintlapizaga@gmail.com>
2023-04-11 11:38:46 -0300

    Update createDatabaseTables.js

--- createDatabaseTables.js
+++ createDatabaseTables.js
@@ -22,8 +22,5 @@ async function createDatabaseTables() {
   await db.sequelize.sync({ force: true });
   console.log("[Database] ¡Las tablas fueron creadas!");
 }
-if (process.env.DB_CONNECTION !== "postgres") {
-  createDatabaseTables();
-}
 
 module.exports = createDatabaseTables;
