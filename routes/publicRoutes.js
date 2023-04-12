049a419bd7a50e7e1f00092b5f5a28a3ab2debef
KChino <kevintlapizaga@gmail.com>
2023-04-12 16:45:29 -0300

    Update publicRoutes.js

--- routes/publicRoutes.js
+++ routes/publicRoutes.js
@@ -1,7 +1,41 @@
 const express = require("express");
 const router = express.Router();
+const transporter = require("../transporter");
 
-// Rutas relacionadas a la parte pública del sitio web:
-// ...
+router.put("/reset", async (req, res) => {
+  try {
+    await require("../createDatabaseTables")();
+    await require("../seeders/categorySeeder")();
+    await require("../seeders/productSeeder")();
+    await require("../seeders/adminSeeder")();
+    await require("../seeders/userSeeder")();
+    console.log("[Database] Reset Completado");
+    return res.status(201).json({
+      message: "Reset database",
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(501).json({
+      message: "Unable to reset database",
+    });
+  }
+});
+
+router.post("/newsletter", async (req, res) => {
+  try {
+    let mailOptions = {
+      from: `"Manos Creativas" <${process.env.EMAIL_USER}>`, // sender address
+      to: req.body.email, // list of receivers
+      subject: "Sobre Nosotros", // Subject line
+      html: `<a href="https://manoscreativas.vercel.app/about-us">https://manoscreativas.vercel.app/about-us</a>
+        `, // html body
+    };
+    await transporter.sendMail(mailOptions);
+    return res.status(200).json("Enviado");
+  } catch (error) {
+    console.log(error);
+    return res.status(404).json(error);
+  }
+});
 
 module.exports = router;
